a453dacc3bf6166e789f0b49a3c859a9da97b63f
Emad Abaei <emad.ae14@gmail.com>
2024-07-12 07:29:44 -0700

    fix: dummy data changed

--- src/data/dummyData.js
+++ src/data/dummyData.js
@@ -8,19 +8,19 @@ export const SPECIALS_DATA = [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum minus deleniti? Nostrum consequatur quae dicta quo neque illo consectetur nam necessitatibus deserunt!"
   },
   {
-    id: "02",
-    image: "/assets/images/bruchetta.png",
-    name: "bruchetta",
-    price: "8.50",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum minus? deserunt!"
-  },
-  {
     id: "03",
     image: "/assets/images/lemon-dessert.png",
     name: "lemon dessert",
     price: "10.00",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum minus deleniti? Nostrum consectetur nam necessitatibus deserunt!"
+  },
+  {
+    id: "02",
+    image: "/assets/images/bruchetta.png",
+    name: "bruchetta",
+    price: "8.50",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum minus? deserunt!"
   }
 ];

d200b144f4c3c2fc56923871d01c4f850f1c78a5
Emad Abaei <emad.ae14@gmail.com>
2024-07-14 04:00:35 -0700

    feat: BookingForm created

---
+++ src/components/BookingForm.js
@@ -0,0 +1,27 @@
+function BookingForm() {
+  return (
+    <form>
+      <label htmlFor='res-date'>Choose date</label>
+      <input type='date' id='res-date' />
+      <label htmlFor='res-time'>Choose time</label>
+      <select id='res-time '>
+        <option>17:00</option>
+        <option>18:00</option>
+        <option>19:00</option>
+        <option>20:00</option>
+        <option>21:00</option>
+        <option>22:00</option>
+      </select>
+      <label htmlFor='guests'>Number of guests</label>
+      <input type='number' placeholder='1' min='1' max='10' id='guests' />
+      <label htmlFor='occasion'>Occasion</label>
+      <select id='occasion'>
+        <option>Birthday</option>
+        <option>Anniversary</option>
+      </select>
+      <input type='submit' value='Make Your reservation' />
+    </form>
+  );
+}
+
+export default BookingForm;
--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,9 +1,12 @@
+// Components
 import Container from "../components/Container";
+import BookingForm from "../components/BookingForm";
 
 function BookingPage() {
   return (
     <Container>
       <h1>Booking Page</h1>
+      <BookingForm />
     </Container>
   );
 }

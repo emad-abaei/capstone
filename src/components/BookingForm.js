3333add93dc161e04476fd98be571e53fe5ac3c1
Emad Abaei <emad.ae14@gmail.com>
2024-07-18 04:19:43 -0700

    fix: BookingForm.test.js changed

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,11 +1,117 @@
-// React Router
-import { useNavigate } from "react-router-dom";
-// Components
-import Button from "./Button";
-// Utils
-import { getToday } from "../utils/helper";
+// // React Router
+// import { useNavigate } from "react-router-dom";
+// // Components
+// import Button from "./Button";
+// // Utils
+// import { getToday } from "../utils/helper";
 
-function BookingForm({
+// function BookingForm({
+//   state,
+//   dispatch,
+//   availableTimes,
+//   setAvailableTimes,
+//   initializeTimes,
+//   submitForm
+// }) {
+//   const navigate = useNavigate();
+//   const today = getToday();
+
+//   function handleDate(e) {
+//     const date = e.target.value;
+//     dispatch({ type: "change_date", payload: date });
+//     initializeTimes(date);
+//   }
+
+//   function handleTime(e) {
+//     dispatch({ type: "change_time", payload: e.target.value });
+//   }
+
+//   function handleGuests(e) {
+//     dispatch({ type: "change_guests", payload: e.target.value });
+//   }
+
+//   function handleOccasion(e) {
+//     dispatch({ type: "change_occasion", payload: e.target.value });
+//   }
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+
+//     const newBooking = {
+//       id: Date.now().toString(),
+//       date: state.date,
+//       time: state.time,
+//       numberOfGuests: state.guests,
+//       occasion: state.occasion
+//     };
+
+//     console.log(newBooking);
+
+//     const submitResponse = submitForm(newBooking);
+
+//     submitResponse && navigate("/confirmation");
+//     setAvailableTimes([]);
+//     dispatch({ type: "reset" });
+//   }
+
+//   return (
+//     <div className='booking'>
+//       <form className='booking__form' onSubmit={handleSubmit}>
+//         <label htmlFor='res-date'>Choose date</label>
+//         <input
+//           type='date'
+//           id='res-date'
+//           data-testid='res-date'
+//           min={today}
+//           value={state?.date}
+//           onChange={handleDate}
+//           required
+//         />
+//         <label htmlFor='res-time'>Choose time</label>
+//         <select
+//           id='res-time'
+//           data-testid='res-time'
+//           value={state?.time}
+//           onChange={handleTime}
+//           disabled={!state?.date}
+//           required>
+//           <option value=''>
+//             {availableTimes?.length ? "Choose a time" : "Select a date first"}{" "}
+//           </option>
+//           {availableTimes?.map((time) => (
+//             <option value={time} key={time}>
+//               {time}
+//             </option>
+//           ))}
+//         </select>
+//         <label htmlFor='guests'>Number of guests</label>
+//         <input
+//           type='number'
+//           placeholder='1'
+//           min='1'
+//           max='10'
+//           id='guests'
+//           value={state?.guests}
+//           onChange={handleGuests}
+//         />
+//         <label htmlFor='occasion'>Occasion</label>
+//         <select id='occasion' value={state?.occasion} onChange={handleOccasion}>
+//           <option value='birthday'>Birthday</option>
+//           <option value='anniversary'>Anniversary</option>
+//         </select>
+//         <Button type='lg'>Make Your reservation</Button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default BookingForm;
+
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+import { getToday, fetchAPI } from "../utils/helper";
+
+export function BookingForm({
   state,
   dispatch,
   availableTimes,
@@ -44,8 +150,6 @@ function BookingForm({
       numberOfGuests: state.guests,
       occasion: state.occasion
     };
-
-    console.log(newBooking);
 
     const submitResponse = submitForm(newBooking);
 
--- src/components/BookingForm.test.js
+++ src/components/BookingForm.test.js
@@ -1,21 +1,29 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import BookingForm from "./BookingForm";
 
+const renderBookingForm = () =>
+  render(
+    <MemoryRouter>
+      <BookingForm />
+    </MemoryRouter>
+  );
+
 test("Renders the Date Label", () => {
-  render(<BookingForm />);
+  renderBookingForm();
   const labelElement = screen.getByText("Choose date");
   expect(labelElement).toBeInTheDocument();
 });
 
 test("Time input is disabled at first", () => {
-  render(<BookingForm />);
+  renderBookingForm();
   const timeInputElement = screen.getByTestId("res-time");
   expect(timeInputElement).toHaveAttribute("disabled");
 });
 
 test("Date input has no value at first", () => {
-  render(<BookingForm />);
+  renderBookingForm();
   const dateInputElement = screen.getByTestId("res-date");
   expect(dateInputElement).toHaveValue("");
 });
